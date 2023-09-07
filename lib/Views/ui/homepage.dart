e16d30d1aaba7ba7854c2b8f53bda16d8530c547
Amantha96 <amanthabandara1996@gmail.com>
2023-09-07 13:08:00 +0530

    9/7/2023

--- lib/Views/ui/homepage.dart
+++ lib/Views/ui/homepage.dart
@@ -10,14 +10,140 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
+  late final TabController _tabController =
+      TabController(length: 3, vsync: this);
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      body: Center(
-        child: Text("This is Home",style: appstyle(40, Colors.black, FontWeight.bold),),
-      ),
+    return Scaffold(
+        body: SizedBox(
+      height: MediaQuery.of(context).size.height,
+      child: Stack(
+        children: [
+          Container(
+            padding: EdgeInsets.fromLTRB(16, 45, 0, 0),
+            height: MediaQuery.of(context).size.height * 0.4,
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage("assets/images/top_image.png"),
+                    fit: BoxFit.fill)),
+            child: Container(
+              padding: EdgeInsets.only(left: 8, bottom: 15),
+              child: SizedBox(
+                width: MediaQuery.of(context).size.width,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Athletics Shoes",
+                      style: appstyleWithHt(
+                          42, Colors.white, FontWeight.bold, 1.5),
+                    ),
+                    Text(
+                      "Collection",
+                      style: appstyleWithHt(
+                          42, Colors.white, FontWeight.bold, 1.2),
+                    ),
+                    TabBar(
+                      indicatorSize: TabBarIndicatorSize.label,
+                      indicatorColor: Colors.transparent,
+                      controller: _tabController,
+                      isScrollable: true,
+                      labelStyle: appstyle(24, Colors.white, FontWeight.bold),
+                      unselectedLabelColor: Colors.grey.withOpacity(0.3),
+                      tabs: [
+                        Tab(text: "Men Shoes"),
+                        Tab(text: "Women Shoes"),
+                        Tab(text: "Kids Shoes"),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(
+                top: MediaQuery.of(context).size.height * 0.25),
+            child: Container(
+              padding: const EdgeInsets.only(left:12),
+              child: TabBarView(controller: _tabController, children: [
+                Column(
+                  children: [
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height * 0.405,
+                      child:ListView.builder(
+                        itemCount: 6,
+                        scrollDirection: Axis.horizontal,
+                        itemBuilder: (context,index){
+                          return Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Container(
+                              color: Colors.grey,
+                              height: MediaQuery.of(context).size.height,
+                              width: MediaQuery.of(context).size.width*0.6,
+                            ),
+                          );
 
-    );
+                        }),
+
+                
+                  
+                
+                    ),
+                    Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              "Latest Shoes",
+                              style: appstyle(24, Colors.black, FontWeight.bold),
+                            ),
+                            Row(
+                              children: [
+                                Text(
+                                  "Show All",
+                                  style:
+                                      appstyle(22, Colors.black, FontWeight.w500),
+                                ),
+                                Icon(
+                                  Icons.arrow_right,
+                                  size: 40,
+                                )
+                              ],
+                            )
+                          ],
+                        ),
+                      ],
+                    ),
+                    Container(
+                      height: MediaQuery.of(context).size.height*0.15,
+                      color: Colors.amber,
+                    )
+                  ],
+                ),
+                Column(
+                  children: [
+                    Container(
+                      height: MediaQuery.of(context).size.height * 0.405,
+                      color: Colors.green,
+                    ),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Container(
+                      height: MediaQuery.of(context).size.height * 0.405,
+                      color: Colors.amber,
+                    ),
+                  ],
+                )
+              ]),
+            ),
+          )
+        ],
+      ),
+    ));
   }
 }
